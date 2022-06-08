98962c07c7d7cb86debd80709caf1c36ece2eb56
sonalvichhivora <98968263+sonalvichhivora@users.noreply.github.com>
2022-06-08 18:40:18 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+// Create function addUser()
+function ____ {
+  // Get value of user by id player1_name_input and player2_name_input
+
+  // Store these values locally
+
+  //Assign "game_page.html" to window.location
+}
+
